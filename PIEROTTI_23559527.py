a0b3587034236d4eaa20af3aa7978279c93a4b41
markpierotti <markpierotti@duck.com>
2024-06-13 10:15:53 +1000

    Update PIEROTTI_23559527.py

--- PIEROTTI_23559527.py
+++ PIEROTTI_23559527.py
@@ -21,20 +21,32 @@ data = pd.read_csv('/Users/pierotti/SCU/ISYS2002_Data Wrangling and Advanced Ana
 #print(f'Number of duplicate rows: {duplicates.sum()}')
 # Number of duplicate rows: 0
 
-# Rename 'dt' column to 'Date' with data.rename from pandas - logic was wrong, relocated data.rename
+# Rename 'dt' column to 'Date' and added spaces to other column headers with data.rename from pandas - logic was wrong, relocated data.rename
 # to where it is now, before index is set
 data.rename(columns = {'dt': 'Date'}, inplace = True)
-# print to check if it works
-print(data)
+data.rename(columns = {'LandAverageTemperature': 'Land Average Temperature'}, inplace = True)
+data.rename(columns = {'LandAverageTemperatureUncertainty': 'Land  Average Temperature Uncertainty'}, inplace = True)
+data.rename(columns = {'LandMaxTemperature': 'Land Max Temperature'}, inplace = True)
+data.rename(columns = {'LandMaxTemperatureUncertainity': 'Land Max Temperature Uncertainity'}, inplace = True)
+data.rename(columns = {'LandMinTemperature': 'Land Min Temperature'}, inplace = True)
+data.rename(columns = {'LandMinTemperatureUncertainity': 'Land Min Temperature Uncertainity'}, inplace = True)
+data.rename(columns = {'LandAndOceanAverageTemperature': 'Land And Ocean Average Temperature'}, inplace = True)
+data.rename(columns = {'LandAndOceanAverageTemperatureUncertainity': 'Land And Ocean Average Temperature Uncertainity'}, inplace = True)
+# print to check if it works. It does work, print code commented out.
+#print(data)
 
 # apply to_datetime to convert date to a string - pd.to_datetime(data['Date']) included hours, 
 # .dt.date attribute added to remove hours, leaving only yyyy-mm-dd
 data['Date'] = pd.to_datetime(data['Date']).dt.date
-print(data)
+# print to see if it works. It does work, print code commented out.
+#print(data)
 #
 
-#apply indexing to column A - 'dt'. pandas library already loaded. note: changed 'dt' to 'Date' to
+# apply indexing to column A - 'Date'. pandas library already loaded. note: changed 'dt' to 'Date' to
 # reflect the change of data.rename relocation in sequence
 data.set_index('Date', inplace=True)
-
+# print to see if it works. It does work, print code commented out. *when code is commented out, index
+# added to dataframe as individual column, code modifies Date column to be Row Label of Dataframe
+# print(data)
+hg
 
